9478aae5867ed0648f06fd4dc86b09c45e56e202
shef <shef@DESKTOP-ILLG190>
2024-03-11 15:15:26 +0200

    Add new class for coding logic

---
+++ ConsoleApp5/Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Logic
+    {
+        public int Power(int i) 
+        {
+            return i * i;
+        }
+    }
+}
